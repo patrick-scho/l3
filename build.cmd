c53e82c6f7120d7f7b5be85ef1103df102cdcde4
Patrick Schönberger <patrick.schoenberger@posteo.de>
2021-02-11 19:47:20 +0100

    build into bin

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 Rem C:/prg/tcc/tcc src/*.c -o main.exe -I src
 
-cl test/parse1.c src/vm/*.c src/parse/*.c src/file/*.c -I src -Zi -w
+cd bin
 
-cl test/vm_fib.c src/vm/*.c src/parse/*.c src/file/*.c -I src -Zi -w
+cl ../test/parse1.c ../src/vm/*.c ../src/parse/*.c ../src/file/*.c -I ../src -Zi -w
+
+cl ../test/vm_fib.c ../src/vm/*.c ../src/parse/*.c ../src/file/*.c -I ../src -Zi -w
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc test/parse1.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w -o parse1
+gcc test/parse1.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w -o bin/parse1
 
-gcc test/vm_fib.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w -o vm_fib
+gcc test/vm_fib.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w -o bin/vm_fib
