426c2c6d283a14dbe927d1185eaa31102916c41b
Patrick Schönberger <patrick.schoenberger@posteo.de>
2021-02-11 19:24:36 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc test/parse1.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w
+gcc test/parse1.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w -o parse1
 
-gcc test/vm_fib.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w
+gcc test/vm_fib.c src/vm/*.c src/parse/*.c src/file/*.c -I src -w -o vm_fib
